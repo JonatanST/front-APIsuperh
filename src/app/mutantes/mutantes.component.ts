e73e67fc7623ac887c78463c247d099d42a0123d
Jonatan Infante <stjhonatan@gmail.com>
2023-04-12 17:11:35 -0500

    arreglos en formularios de registro de mutantes, iconos y otras cosas de diseño y ortografia

--- src/app/mutantes/mutantes.component.ts
+++ src/app/mutantes/mutantes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-mutantes',
@@ -20,7 +21,40 @@ export class MutantesComponent {
   ngOnInit() {
     this.http.get('http://localhost:3000/seres').subscribe((data: any) => {
       this.datos = data;
-      console.log(this.datos); // Verifica los datos en la consola
+      console.log(this.datos); // Ver los datos en la consola
+    });
+  }
+  /* Metodo para editar datos nombre y ciudad del mutante*/
+  editarSeres(item: any) {
+    Swal.fire({
+      title: 'Editar ser',
+      html:
+        '<label>Nombre:</label><br><input id="swal-input1" class="swal2-input" style="display:flex" value="' +
+        item.nombre +
+        '">' +
+        '<label>Ciudad:</label><br><input id="swal-input2" class="swal2-input" style="display:flex" value="' +
+        item.ciudad +
+        '">',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          (document.getElementById('swal-input1') as HTMLInputElement).value,
+          (document.getElementById('swal-input2') as HTMLInputElement).value,
+        ];
+      },
+    }).then((result: any) => {
+      if (result.value) {
+        item.nombre = result.value[0];
+        item.ciudad = result.value[1];
+        this.http.put(`http://localhost:3000/seres/${item.id}`, item).subscribe(
+          (response: any) => {
+            console.log(response);
+          },
+          (error: any) => {
+            console.log(error);
+          }
+        );
+      }
     });
   }
 }
